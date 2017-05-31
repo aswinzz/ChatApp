418b02c37ae8efb32777a28f99fca46329f96ba3
AVB <iit2016106@iiita.ac.in>
2017-05-31 17:09:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-users=[];
-connections=[];
+var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-/*
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-*/
-io.on('connection', function(socket){
-	connections.push(socket);
-	console.log("connected: %s connected",connections.length);
-    socket.on("disconnect",function(data){
-       connections.splice(connections.indexOf(socket),1);
-       console.log("disconnected: %s sockets connected",connections.length);
-    });
-     socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
